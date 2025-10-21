6f35df9f947da149cda236ed33075b4fc3743e5a
Gabriel <liberatogames109@gmail.com>
2025-10-21 13:59:33 -0300

    FEATURE - Add folders structure

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { server } from './server/Server';
 
 
 
-server.listen(3333, () => console.log("app rodando!"));
+server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+});
