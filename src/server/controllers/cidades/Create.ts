b1a9eb08889829b10eff2dadc0c9710269dd1bce
Gabriel <liberatogames109@gmail.com>
2025-10-21 15:01:56 -0300

    UPDATE - add YUP lib

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
     nome:string
 }
 
 export const Create = (req: Request<{},{},ICidade>, res: Response) => {
-
+    if (req.body.nome === undefined){
+        return res.status(StatusCodes.BAD_REQUEST).send("informe o nome")
+    } else if(req.body.nome.length < 3 ){
+        return res.status(StatusCodes.BAD_REQUEST).send("O atributo nome precisa ter mais de 3 caracteres")
+    }
     console.log(req.body.nome);
 
     return res.send("Create!");
