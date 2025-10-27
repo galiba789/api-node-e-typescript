c954e35fc009a3f7a771f44997f63344c275cb4c
Gabriel <liberatogames109@gmail.com>
2025-10-27 11:59:30 -0300

    UPDATE - Adding Jest

--- build/server/routes/index.js
+++ build/server/routes/index.js
@@ -2,13 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
+const controllers_1 = require("./../controllers");
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/', (_, res) => {
     return res.send("Olá, dev!");
 });
-router.post('/teste', (req, res) => {
-    console.log(req.cookies);
-    return res.status(http_status_codes_1.StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.get('/cidades', controllers_1.cidadesController.getAllValidation, controllers_1.cidadesController.getAll);
+router.post('/cidades', controllers_1.cidadesController.createValidation, controllers_1.cidadesController.Create);
+router.get('/cidades/:id', controllers_1.cidadesController.getByIdValidation, controllers_1.cidadesController.getById);
+router.put('/cidades/:id', controllers_1.cidadesController.updateByIdValidation, controllers_1.cidadesController.UpdateById);
+router.delete('/cidades/:id', controllers_1.cidadesController.deleteByIdValidation, controllers_1.cidadesController.deleteById);
